3aa50902ca1c147e93f03be02f61a2a64fb65a04
olly-evans <evansoliver345@gmail.com>
2025-11-10 11:28:59 +0000

    initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "editor.hpp"
+
+int main(void) {
+  std::cout << "Hello, World!\n";
+  return 0;
+}
---
+++ terminal.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void die(std::string error) {
+    
+    std::cerr << error << std::endl;
+}
